080997f901dd12007be098e8d0dae67262d1be10
pero786 <sarecp3@gmail.com>
2025-03-17 09:18:16 +0100

    postavaAPPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Route, Router } from "@solidjs/router";
-import { AuthProvider } from "./components/AuthProvider";
+import { AuthProvider, useAuth } from "./components/AuthProvider";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import Error from "./pages/Error";
+import { Show } from "solid-js";
+import { A } from "@solidjs/router";
 
 
 export default function App() {
@@ -19,14 +21,25 @@ export default function App() {
 }
 
 function Layout(props) {
-
+  const user = useAuth();
 
 
   return (
-    <div>
-      <div>Zaglavlje</div>
-      <div>{props.children}</div>
-      <div>Podnožje</div>
-    </div>
-  );
+    <>
+
+      <header class=" flex flex-row gap-2 items-center p-2">
+        <div class="flex-none">Zaglavlje</div>
+        <nav class="flex-1 pl text-right">
+          <Show when={user()}>
+            Odjava
+          </Show>
+          <Show when={!user()}>
+            <A class="p-2 bg-amber-500 text-gray-50 font-bold rounded hover:brightness-120" href="/signin">Prijava</A>
+          </Show>
+        </nav>
+      </header>
+      <main>{props.children}</main>
+      <footer>Podnožje</footer>
+    </>
+    );
 }
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,24 @@
 export default function SignIn() {
+    function formSubmit() { };
 
-    return (<div>Prijava korisnika</div>);
+    return (
+        <>
+            <div class=" text-3xl font-mono font-bold">Prijava korisnika</div>
+            <form onSubmit={formSubmit} class="w-md">
+                <div class="p-2 flex flex-col gap-1">
+                    <label> E-mail adresa: </label>
+                    <input class="border rounded p-2" type="email" name="email" required="true" />
+                </div>
+
+                <div class="p-2 flex flex-col gap-1">
+                    <label> Zaporka: </label>
+                    <input class="border rounded p-2" type="password" name="password" required="true" min="6" />
+                </div>
+
+                <div class="p-2 flex flex-col gap-1">
+                    <input class="border rounded p-2 bg-slate-600 hover:brightness-120" type="submit" value="Pošalji" />
+                </div>
+            </form>
+        </>
+    )
 }
